37f01f4fedef60cd6072258bd76f95a31dffa723
hexu <hexu_1985@sina.com>
2017-07-25 02:34:42 +0800

    add start_stunserver.txt and tools

---
+++ tools/udpechoclient2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# UDP Example - Chapter 2
+
+import socket, sys, time
+
+if len(sys.argv) < 3 or len(sys.argv) > 4:
+    print "usage: udpechoclient01.py <hostname> <service> [localport]"
+    sys.exit(1)
+
+host = sys.argv[1]
+textport = sys.argv[2]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+if len(sys.argv) == 4:
+    local_port = int(sys.argv[3])
+    s.bind(("", local_port))
+
+try:
+    port = int(textport)
+except ValueError:
+    # That didn't work.  Look it up instread.
+    port = socket.getservbyname(textport, 'udp')
+
+s.connect((host, port))
+print "Looking for replies; press Ctrl-C or Ctrl-Break to stop."
+while 1:
+    data = sys.stdin.readline().strip()
+    if not len(data):
+        break
+    s.sendall(data)
+    buf = s.recv(2048)
+    if not len(buf):
+        break
+    print "Received: %s" % buf
+
+s.close()
